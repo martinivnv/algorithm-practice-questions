9df1abdaaa9ff603eef1cf0771832fc05ebd8c1c
martin20185 <43828876+martin20185@users.noreply.github.com>
2021-07-23 16:48:57 -0400

    Urlify problem added (unsolved)

---
+++ Arrays/urlify.py
@@ -0,0 +1,12 @@
+# Write a method to replace all spaces in a string with '%20'. You may assume that the string
+# has sufficient space at the end to hold the additional characters, and that you are given the "true"
+# length of the string.
+# 07 23 2021
+
+# Hints:
+# It's often easiest to modify strings by going from the end of the string to the beginning. 
+# You might find you need to know the number of spaces. Can you just count them?
+
+class Solver(object):
+    def __init__(self):
+        pass
