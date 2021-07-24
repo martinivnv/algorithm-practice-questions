0dad629b3f27f614d68963e278d0fae9bab9cfde
martin20185 <43828876+martin20185@users.noreply.github.com>
2021-07-24 11:50:44 -0400

    URLify problem solved

--- Arrays/urlify.py
+++ Arrays/urlify.py
@@ -10,3 +10,33 @@
 class Solver(object):
     def __init__(self):
         pass
+
+    # O(N^2) time and O(1) space, N being the true length of the string
+    def urlify(self, str, true_length):
+        num_spaces = 0
+        # Count number of spaces in the "true length" part of the string
+        for i in range(true_length):
+            if str[i] == " ":
+                num_spaces += 1
+
+        # Find index of last character in the final string
+        index = true_length + num_spaces * 2
+        str_list = list(str)
+        # Loop through the "true string" backwards
+        for j in range(true_length-1, -1, -1):
+            # When a space is found, replace it
+            if str_list[j] == " ":
+                str_list[index-1] = "0"
+                str_list[index-2] = "2"
+                str_list[index-3] = "%"
+                index = index - 3
+            # If the character is not a space, move it to the end of the string list
+            else:
+                str_list[index - 1] = str_list[j]
+                index -= 1
+        
+        return "".join(str_list)
+
+if __name__ == "__main__":
+    s = Solver()
+    print(s.urlify("Mr John Smith    ", 13))
