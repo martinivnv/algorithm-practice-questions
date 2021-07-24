6cea3ca37a0af624dd736facf844fe670aa5050f
martin20185 <43828876+martin20185@users.noreply.github.com>
2021-07-24 12:36:21 -0400

    Palindrome permutation problem solved

---
+++ Arrays/palindrome_permutation.py
@@ -0,0 +1,53 @@
+# Given a string, write a function to check if it is a permutation of a palindrome. 
+# A palindrome is a word or phrase that is the same forwards and backwards. A permutation
+# is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+# 07 24 2021
+
+# Hints:
+# You do not have to-and should not-generate all permutations. This would be very inefficient. 
+# What characteristics would a string that is a permutation of a palindrome have?
+# Have you tried a hash table? You should be able to get this down to O(N) time. 
+# Can you reduce the space usage by using a bit vector? 
+
+class Solver(object):
+    def __init__(self):
+        pass
+
+    def check(self, str):
+        if len(str) == 1:
+            return True
+
+        chars = {}
+        for letter in str:
+            letter = letter.lower()
+            if letter in chars:
+                chars[letter] = not chars[letter]
+            else:
+                chars[letter] = False
+        print(chars)
+        if len(str) % 2 == 0:
+            return self.check_even(chars)
+        else:
+            return self.check_odd(chars)
+    
+    def check_even(self, chars):
+        for letter, bool in chars.items():
+            if bool is False:
+                return False
+        return True
+
+    def check_odd(self, chars):
+        middle_found = False
+        for letter, bool in chars.items():
+            if bool is False:
+                if not middle_found:
+                    middle_found = True
+                else:
+                    return False
+        return True
+
+if __name__ == "__main__":
+    s = Solver()
+    print(s.check("racecar"))
+    print(s.check("zzz"))
+    print(s.check("cdcdcd"))
