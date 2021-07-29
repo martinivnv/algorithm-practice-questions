516373b8ce7e8992f0255de489942a2ec29d493a
martin20185 <43828876+martin20185@users.noreply.github.com>
2021-07-29 12:52:13 -0400

    Implemented binary search tree

---
+++ Trees and Graphs/binary_search_tree.py
@@ -0,0 +1,32 @@
+# Implement a binary tree from scratch
+# Assume no duplicates allowed
+# 07 29 2021
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+            else:
+                self.data = data
+
+if __name__ == "__main__":
+    tree = Node(8)
+    tree.insert(4)
+    tree.insert(10)
+    tree.insert(2)
+    tree.insert(6)
+    tree.insert(20)
